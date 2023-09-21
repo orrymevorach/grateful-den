c48e6651efa7c1f90bffe169fa9f80f342f5da53
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2023-09-21 12:43:20 -0400

    description text with testing

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,7 +1,45 @@
 import styles from './card.module.scss';
 import Image from 'next/image';
 import logo from 'public/logo.JPEG';
+import { useState } from 'react';
 
 export default function Card() {
-  return <Image src={logo} alt="" className={styles.card} />;
+  const [width, setWidth] = useState('400px');
+  return (
+    <>
+      <div style={{ position: 'absolute', top: '0px', left: '0px' }}>
+        <button
+          style={{
+            background: 'none',
+            fontSize: '16px',
+          }}
+          onClick={() => setWidth('400px')}
+        >
+          Small
+        </button>
+        <button
+          style={{
+            background: 'none',
+            fontSize: '16px',
+          }}
+          onClick={() => setWidth('450px')}
+        >
+          Medium
+        </button>
+        <button
+          style={{
+            background: 'none',
+            fontSize: '16px',
+          }}
+          onClick={() => setWidth('500px')}
+        >
+          Large
+        </button>
+      </div>
+      <div className={styles.cardContainer} style={{ width }}>
+        <Image src={logo} alt="" className={styles.card} />
+        <p className={styles.description}>A social wellness club</p>
+      </div>
+    </>
+  );
 }
