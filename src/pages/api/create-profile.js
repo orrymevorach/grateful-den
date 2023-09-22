8b3a8f621f99b5c2aadaff108e9b3f59c7af262a
orrymevorach <38357766+orrymevorach@users.noreply.github.com>
2023-09-22 12:31:57 -0400

    testing

--- src/pages/api/create-profile.js
+++ src/pages/api/create-profile.js
@@ -1,37 +1,45 @@
 export default async function handler(req, res) {
   try {
     console.log('hey dude!');
-    // const { email } = req.body;
-    // const date = new Date();
-    // const revision = date.toISOString().split('T')[0];
-    // const profileOptions = {
-    //   method: 'POST',
-    //   headers: {
-    //     accept: 'application/json',
-    //     revision,
-    //     'content-type': 'application/json',
-    //     Authorization: `Klaviyo-API-Key ${process.env.KLAVIYO_API_KEY}`,
-    //   },
-    //   body: JSON.stringify({
-    //     data: {
-    //       type: 'profile',
-    //       attributes: {
-    //         email,
-    //       },
-    //     },
-    //   }),
-    // };
-    // const profile = await (
-    //   await fetch('https://a.klaviyo.com/api/profiles/', profileOptions)
-    // ).json();
+    const { email } = req.body;
+    const date = new Date();
+    const revision = date.toISOString().split('T')[0];
+    const profileOptions = {
+      method: 'POST',
+      headers: {
+        accept: 'application/json',
+        revision,
+        'content-type': 'application/json',
+        Authorization: `Klaviyo-API-Key ${process.env.KLAVIYO_API_KEY}`,
+      },
+      body: JSON.stringify({
+        data: {
+          type: 'profile',
+          attributes: {
+            email,
+          },
+        },
+      }),
+    };
+    const profile = await (
+      await fetch('https://a.klaviyo.com/api/profiles/', profileOptions)
+    ).json();
 
-    // if (!!profile.errors) {
-    //   res.status(200).json({ errors: profile.errors });
-    // } else {
-    //   res.status(200).json({ profile });
-    // }
-    res.status(200).json({});
+    if (!!profile.errors) {
+      res.status(200).json({ errors: profile.errors });
+    } else {
+      res.status(200).json({ profile });
+    }
   } catch (err) {
     res.status(err.statusCode || 500).json({ errors: err.message });
   }
 }
+
+export const config = {
+  api: {
+    // disable nextjs's body parser while deployed
+    // (as body parsing is handled by `https.onRequest()`),
+    // but enable it for local development using `next dev`
+    bodyParser: process.env.NODE_ENV !== 'production',
+  },
+};
--- src/pages/api/testing.js
+++ src/pages/api/testing.js
@@ -5,3 +5,12 @@ export default async function handler(req, res) {
     res.status(err.statusCode || 500).json({ errors: err.message });
   }
 }
+
+export const config = {
+  api: {
+    // disable nextjs's body parser while deployed
+    // (as body parsing is handled by `https.onRequest()`),
+    // but enable it for local development using `next dev`
+    bodyParser: process.env.NODE_ENV !== 'production',
+  },
+};
